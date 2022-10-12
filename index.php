3c254f7833173e1738f141a85024f70fead2ef7f
Krzychurz <bartekrz66@gmail.com>
2022-10-12 11:41:02 +0200

    ostatnia zmiana

--- index.php
+++ index.php
@@ -7,8 +7,7 @@
     <body>
         <?php
             $con = new mysqli("51.123.34.245","admin","123456","quiz");
-            $sql = "SELECT a.content AS answer, a.is_right, q.content AS question FROM answers AS a JOIN questions AS q ON a.questions_id = q.id";
-
+            $sql = "SELECT q.content as question, a.content as answer FROM questions_has_answers as qhs JOIN questions as q "
             $res = $con->query($sql);
             $row = $res->fetch_all(MYSQLI_ASSOC);
 
